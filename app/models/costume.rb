d1048ff36d52b14a556bf8840aedd9f8abfbe35f
Christia T <christiatidwell@bellsouth.net>
2018-08-19 04:08:48 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,10 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class CreateDogs < ActiveRecord::Base
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+end
