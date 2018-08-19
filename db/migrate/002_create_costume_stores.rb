f9eea18ec2f20dc08f3adc1272dcff5ed7d39fff
Christia T <christiatidwell@bellsouth.net>
2018-08-19 05:10:09 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStore < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.string :location 
-      t.string :size
+      t.integer :number of costumes
       t.binary :image_url
       t.timestamp :created_at
       t.timestamp :updated_at
