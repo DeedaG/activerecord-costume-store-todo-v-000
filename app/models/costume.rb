fe86e9861c3990c5fac8a591829fe1e7664318af
Christia T <christiatidwell@bellsouth.net>
2018-08-19 04:27:09 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,10 +1,2 @@
-class CreateCostume < ActiveRecord::Base
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.string :price
-      t.integer :size
-      t.string :imageurl
-    end
-  end
+class Costume
 end
