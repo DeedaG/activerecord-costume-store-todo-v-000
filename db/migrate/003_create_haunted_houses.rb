0adf1afe19b097462f5e5a94904e4518adc0869b
Christia T <christiatidwell@bellsouth.net>
2018-08-19 05:53:43 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.string :price
-      t.boolean :still_in_business
+      t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
     end
